eeb9ac424f09c9bce2641b3b399f79fa4afa742b
Charles <cming.chou@gmail.com>
2017-04-19 21:51:35 +0000

    added POST and GET using ajax to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,58 +4,33 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(function onReady() {
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "<script>alert('uh oh!');</script>",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
 
   Handlebars.registerHelper('fromNow', (time) => moment(time).fromNow());
-
   const tempText = $('#tweet-temp').text();
   const templateCompiled = Handlebars.compile(tempText);
+  const $form = $('.container form');
 
-  $(".tweet-container").append(data.map(templateCompiled));
+  function renderTweets(data){
+    $('.tweet-container').empty();
+    $('.tweet-container').prepend(data.reverse().map(templateCompiled));
+  }
 
-})
+  function loadTweets (){
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+    }).then((data) => renderTweets(data));
+  }
+
+  $form.on('submit', function () {
+    event.preventDefault();
+
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $(this).serialize()
+      }).then(loadTweets());
+    });
+
+  loadTweets();
+});
