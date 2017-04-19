44143f7c455efbd550fcdc6b26b0b46520a16028
Charles <cming.chou@gmail.com>
2017-04-19 07:43:53 +0000

    added dynamic tweet article in app.js using template strings and updated css for spacing between tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,79 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(function onReady() {
+  var data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "<script>alert('uh oh!');</script>",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  function createTweetElement (data){
+    return `
+    <article class="tweet">
+      <header>
+        <img class="profile-pic" src="${escape(data.user.avatars.small)}">
+        <h2 class="profile-name">${escape(data.user.name)}</h2>
+        <p class="handle">${escape(data.user.handle)}</p>
+      </header>
+      <p>${escape(data.content.text)}</p>
+      <footer class="clearfix">
+        <p class="tweet-date">${moment(data.created_at).fromNow()}</p>
+        <div class="icons">
+          <i class="fa fa-heart" aria-hidden="true"></i>
+          <i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+        </div>
+      </footer>
+    </article>`;
+  }
+  $(".tweet-container").append(data.map(createTweetElement));
+
+});
